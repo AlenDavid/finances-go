30a8d41be3a1429d67ede792953069bd21d4c6a6
David Alen <davidalen.dev@gmail.com>
2022-07-04 04:14:49 -0300

    add: some sort of query interaction

--- main.go
+++ main.go
@@ -5,10 +5,16 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		message := "pong"
+		value, exists := c.GetQuery("message")
+
+		if exists {
+			message = value
+		}
+
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": message,
 		})
 	})
 	r.Run() // listen and server on 0.0.0.0:8080
 }
-
